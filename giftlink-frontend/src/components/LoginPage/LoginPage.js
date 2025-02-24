ae14c0f98733d99bf5221899677c194734f80c8d
DaviVitoreti <DaviVitoreti@outlook.com>
2025-02-24 05:25:01 -0500

    Changed LoginPage.js

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,10 +1,23 @@
 import React, { useState } from 'react';
 import './LoginPage.css';
+import {urlConfig} from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 function LoginPage() {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [incorrect, setIncorrect] = useState('');
+    const navigate = useNavigate();
+    const bearerToken = sessionStorage.getItem('bearer-token');
+    const { setIsLoggedIn } = useAppContext();
+
+    useEffect(() => {
+        if (sessionStorage.getItem('auth-token')) {
+        navigate('/app')
+        }
+    }, [navigate])
 
     const handleLogin = async () => {
         console.log("Inside handleLogin");
@@ -39,6 +52,45 @@ function LoginPage() {
         </div>
       </div>
     )
+}
+
+const handleLogin = async () => {
+    try{
+        //first task
+      const response = await fetch(`/api/auth/login`, {
+           method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': bearerToken ? `Bearer ${bearerToken}` : '', // Include Bearer token if available
+            },
+            body: JSON.stringify({    
+                email: email,
+                password: password,
+            })
+     })
+      }catch (e) {
+        console.log("Error fetching details: " + e.message);
+
+        const json = await res.json();
+        sessionStorage.setItem('auth-token', json.authtoken);
+        sessionStorage.setItem('name', json.userName);
+        sessionStorage.setItem('email', json.userEmail);
+        setIsLoggedIn(true);
+        navigate('/app')
+
+        if (json.authtoken) {
+            // Tasks 1-4 done previously
+            } else {
+                document.getElementById("email").value="";
+                document.getElementById("password").value="";
+                setIncorrect("Wrong password. Try again.");
+            //Below is optional, but recommended - Clear out error message after 2 seconds
+                setTimeout(() => {
+                    setIncorrect("");
+                }, 2000);
+        }
+        <span style={{color:'red',height:'.5cm',display:'block',fontStyle:'italic',fontSize:'12px'}}>{incorrect}</span>
+    }
 }
 
 export default LoginPage;
